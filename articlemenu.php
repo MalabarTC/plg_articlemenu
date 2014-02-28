b4948dd59adecc59b85c4f541687d87a005d6461
Jan Linhart <jan.linhart@easysoftware.cz>
2014-02-28 10:24:56 +0100

    Prepared plugin structure

---
+++ articlemenu.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Content.articlemenu
+ *
+ * @copyright   Copyright (C) EasyJoomla.org. All rights reserved.
+ * @license     GNU General Public License version 2 or later
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Plugin create anchor menu from an article generated form HTML headings.
+ */
+class PlgContentArticlemenu extends JPlugin
+{
+
+}
